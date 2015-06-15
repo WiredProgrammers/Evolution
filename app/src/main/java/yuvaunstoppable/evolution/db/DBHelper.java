c60baa5065950deb4784b7287013b1eaa05f3238
yash2710 <13bce123@nirmauni.ac.in>
2015-06-15 13:57:01 +0530

    sanitation row

---
+++ app/src/main/java/yuvaunstoppable/evolution/db/DBHelper.java
@@ -0,0 +1,7 @@
+package yuvaunstoppable.evolution.db;
+
+/**
+ * Created by Yash on 11-Jun-15.
+ */
+public class DBHelper {
+}
--- app/src/main/java/yuvaunstoppable/evolution/fragments/VolunteerSanitation.java
+++ app/src/main/java/yuvaunstoppable/evolution/fragments/VolunteerSanitation.java
@@ -3,9 +3,12 @@ package yuvaunstoppable.evolution.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import yuvaunstoppable.evolution.R;
 
 /**
  * Created by amit on 10-Jun-15.
@@ -16,9 +19,13 @@ public class VolunteerSanitation extends Fragment {
     public VolunteerSanitation(){
 
     }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View layout = inflater.inflate(R.layout.fragment_volunteer_sanitation,container,false);
+        RecyclerView recyclerView = (RecyclerView) layout.findViewById(R.id.list);
+
+        return layout;
     }
 }
