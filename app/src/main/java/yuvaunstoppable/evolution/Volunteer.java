2d6578731bc61e9810aafd3c343b7adbaeeb53f5
yash2710 <13bce123@nirmauni.ac.in>
2015-06-18 14:11:11 +0530

    volunteer logout

--- app/src/main/java/yuvaunstoppable/evolution/Volunteer.java
+++ app/src/main/java/yuvaunstoppable/evolution/Volunteer.java
@@ -8,6 +8,10 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 import yuvaunstoppable.evolution.fragments.VolunteerCampusRenovation;
 import yuvaunstoppable.evolution.fragments.VolunteerOthers;
@@ -47,6 +51,29 @@ public class Volunteer extends AppCompatActivity{
 
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.logout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            ParseUser.logOut();
+            startActivity(new Intent(Volunteer.this,Main.class));
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
     public class volunteerViewPagerAdapter extends FragmentStatePagerAdapter{
         String tabs[];
