39b523ac73ec24f003879243686d5be27b8bb863
Amit <amitsharma13110@gmail.com>
2015-06-18 16:16:43 +0530

    water xml and java

--- app/src/main/java/yuvaunstoppable/evolution/fragments/VolunteerWater.java
+++ app/src/main/java/yuvaunstoppable/evolution/fragments/VolunteerWater.java
@@ -3,10 +3,15 @@ package yuvaunstoppable.evolution.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import yuvaunstoppable.evolution.NothingSelectedSpinnerAdapter;
 import yuvaunstoppable.evolution.R;
 
 /**
@@ -17,11 +22,17 @@ public class VolunteerWater extends Fragment {
     public VolunteerWater(){
 
     }
+
+    String[] types  = {"1 month","2 months","3 months","6 months","12 months"};
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_volunteer_water,container,false);
-
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.spinner2_item, types);
+        Log.d("Adapter", String.valueOf(adapter.isEmpty()));
+        final Spinner how_often_clean = (Spinner) layout.findViewById(R.id.how_often_clean);
+        how_often_clean.setPrompt("Select no. of months");
+        how_often_clean.setAdapter(new NothingSelectedSpinnerAdapter(adapter,R.layout.spinner2_item,getActivity()));
         return layout;
     }
 }
