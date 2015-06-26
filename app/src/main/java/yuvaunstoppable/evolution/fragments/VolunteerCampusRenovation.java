18d2b830ed271f161ec3a7b9012bd3a044e5e766
yash2710 <13bce123@nirmauni.ac.in>
2015-06-26 15:48:34 +0530

    renovation

--- app/src/main/java/yuvaunstoppable/evolution/fragments/VolunteerCampusRenovation.java
+++ app/src/main/java/yuvaunstoppable/evolution/fragments/VolunteerCampusRenovation.java
@@ -5,6 +5,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 import yuvaunstoppable.evolution.R;
 
@@ -17,10 +20,20 @@ public class VolunteerCampusRenovation extends android.support.v4.app.Fragment {
     public VolunteerCampusRenovation(){
 
     }
+    CheckBox needRepair,clean;
+    EditText noBlackboard,noDustbin,comments;
+    RatingBar starBlackboard,starColor;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_volunteer_campus_renovation,container,false);
+        needRepair = (CheckBox) layout.findViewById(R.id.need_repair);
+        clean = (CheckBox) layout.findViewById(R.id.clean);
+        noBlackboard = (EditText) layout.findViewById(R.id.no_blackboard);
+        noDustbin = (EditText) layout.findViewById(R.id.no_dustbin);
+        comments = (EditText) layout.findViewById(R.id.comments);
+        starBlackboard = (RatingBar) layout.findViewById(R.id.star_blackboard);
+        starColor = (RatingBar) layout.findViewById(R.id.star_color);
 
         return layout;
     }
