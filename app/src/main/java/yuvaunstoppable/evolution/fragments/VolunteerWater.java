95ca71188ed4c53b33dbbb79d006f62f31791aa4
yash2710 <13bce123@nirmauni.ac.in>
2015-06-26 15:16:47 +0530

    others

--- app/src/main/java/yuvaunstoppable/evolution/fragments/VolunteerWater.java
+++ app/src/main/java/yuvaunstoppable/evolution/fragments/VolunteerWater.java
@@ -3,11 +3,14 @@ package yuvaunstoppable.evolution.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SwitchCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -23,11 +26,21 @@ public class VolunteerWater extends Fragment {
 
     }
 
+    SwitchCompat regularCleaningTank,waterPureProper,regularFlowWater,tapLeakage,drainClog,stinking;
+    EditText fillTankFreq,watertankCapacity,noBrokenTaps,noDustbins,comments;
+    TextView noOfTaps;
+    RatingBar starCleanAroundWater;
     String[] types  = {"1 month","2 months","3 months","6 months","12 months"};
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_volunteer_water,container,false);
+        regularCleaningTank = (SwitchCompat) layout.findViewById(R.id.regularity_cleaning_tank);
+        waterPureProper = (SwitchCompat) layout.findViewById(R.id.water_pure_proper);
+        regularFlowWater = (SwitchCompat) layout.findViewById(R.id.reg_flow_water);
+        tapLeakage = (SwitchCompat) layout.findViewById(R.id.tap_Leakage);
+        drainClog = (SwitchCompat) layout.findViewById(R.id.drain_clogg);
+        stinking = (SwitchCompat) layout.findViewById(R.id.stinking);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.spinner2_item, types);
         Log.d("Adapter", String.valueOf(adapter.isEmpty()));
         final Spinner how_often_clean = (Spinner) layout.findViewById(R.id.how_often_clean);
