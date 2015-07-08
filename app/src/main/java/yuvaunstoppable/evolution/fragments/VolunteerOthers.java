989f10c73190475a2cfc20cb02f4ced545080878
Amit <amitsharma13110@gmail.com>
2015-07-08 12:53:18 +0530

    others final

--- app/src/main/java/yuvaunstoppable/evolution/fragments/VolunteerOthers.java
+++ app/src/main/java/yuvaunstoppable/evolution/fragments/VolunteerOthers.java
@@ -3,9 +3,11 @@ package yuvaunstoppable.evolution.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.RatingBar;
 
 import org.apache.http.NameValuePair;
@@ -24,6 +26,8 @@ public class VolunteerOthers extends Fragment {
 
     }
     static RatingBar starCleanMidDay,starCleanCampus;
+    static SwitchCompat soundSystem, kitchenMiddayMeals, sportsKits, booksStationery, amenitiesCultural;
+    static EditText  otherComments;
 
     @Nullable
     @Override
@@ -32,6 +36,12 @@ public class VolunteerOthers extends Fragment {
 
         starCleanCampus = (RatingBar) layout.findViewById(R.id.star_clean_campus);
         starCleanMidDay = (RatingBar) layout.findViewById(R.id.star_clean_midday);
+        soundSystem = (SwitchCompat) layout.findViewById(R.id.sound_system);
+        kitchenMiddayMeals = (SwitchCompat) layout.findViewById(R.id.kitchen_midday_meals);
+        sportsKits = (SwitchCompat) layout.findViewById(R.id.sports_kits);
+        booksStationery = (SwitchCompat) layout.findViewById(R.id.books_stationery);
+        amenitiesCultural = (SwitchCompat) layout.findViewById(R.id.amenities_cultural);
+        otherComments = (EditText) layout.findViewById(R.id.other_comments);
 
         return layout;
     }
@@ -41,6 +51,13 @@ public class VolunteerOthers extends Fragment {
         try {
             values.add(new BasicNameValuePair("clean_midday", Integer.toString((int) starCleanMidDay.getRating())));
             values.add(new BasicNameValuePair("clean_campus", Integer.toString((int) starCleanCampus.getRating())));
+            values.add(new BasicNameValuePair("sound system", soundSystem.isChecked()? "Yes" : "No"));
+            values.add(new BasicNameValuePair("kitchen midday meals", kitchenMiddayMeals.isChecked()? "Yes" : "No"));
+            values.add(new BasicNameValuePair("sports kits", sportsKits.isChecked()? "Yes" : "No"));
+            values.add(new BasicNameValuePair("books stationery", booksStationery.isChecked()? "Yes" : "No"));
+            values.add(new BasicNameValuePair("amenities cultural", amenitiesCultural.isChecked()? "Yes" : "No"));
+            values.add(new BasicNameValuePair("Comments", otherComments.getText().toString()));
+
         }catch(Exception e){}
         return values;
     }
