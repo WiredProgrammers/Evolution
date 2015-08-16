e42b310d313f5ec5ffe878a08d6cf17b4026c672
yash2710 <13bce123@nirmauni.ac.in>
2015-08-16 16:02:46 +0530

    lkjlm

--- app/src/main/java/yuvaunstoppable/evolution/fragments/AdminView.java
+++ app/src/main/java/yuvaunstoppable/evolution/fragments/AdminView.java
@@ -16,6 +16,8 @@ public class AdminView extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_adminview, container, false);
+        View layout = inflater.inflate(R.layout.fragment_adminview, container, false);
+
+        return layout;
     }
 }
