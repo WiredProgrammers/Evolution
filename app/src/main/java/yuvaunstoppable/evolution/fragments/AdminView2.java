3afb7ec8dfb535c7bcc8863f9bbbb1910895eca7
Amit <amitsharma13110@gmail.com>
2015-10-02 14:33:08 +0530

    text_size=20ps

---
+++ app/src/main/java/yuvaunstoppable/evolution/fragments/AdminView2.java
@@ -0,0 +1,22 @@
+package yuvaunstoppable.evolution.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import yuvaunstoppable.evolution.R;
+
+/**
+ * Created by amit on 02-Oct-15.
+ */
+public class AdminView2 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_adminview2, container, false);
+
+
+        return layout;
+    }
+}
